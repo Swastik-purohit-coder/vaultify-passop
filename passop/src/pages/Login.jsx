30bc50b36d26fe4d6f8eaa0c6daac99a5de55c4d
swastik <swastikpurohit2007@gmail.com>
2026-02-18 00:16:34 +0530

    add

--- passop/src/pages/Login.jsx
+++ passop/src/pages/Login.jsx
@@ -25,7 +25,7 @@ const Login = () => {
 
       localStorage.setItem("token", res.data.token);
 
-      navigate("/");
+      navigate("/dashboard");
     } catch (err) {
       setError("Invalid email or password");
     }
